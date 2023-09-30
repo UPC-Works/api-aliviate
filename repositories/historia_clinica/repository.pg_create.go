1016f9cdc09da2fd74a8e98bf816df53c5c7df67
Edward L. Lua <87093989+e-lua@users.noreply.github.com>
2023-09-29 19:38:17 -0500

    chore: Retorno de id de HC al momento de crearse

--- repositories/historia_clinica/repository.pg_create.go
+++ repositories/historia_clinica/repository.pg_create.go
@@ -8,7 +8,7 @@ import (
 	models "github.com/UPC-Works/api-aliviate/models"
 )
 
-func Pg_Create(input_historia_clinica *models.HistoriaClinica) error {
+func Pg_Create(input_historia_clinica *models.HistoriaClinica) (string, error) {
 
 	//Context time limit
 	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
@@ -91,7 +91,7 @@ func Pg_Create(input_historia_clinica *models.HistoriaClinica) error {
 		tiene_hab_alcohol                 ,
 		tiene_hab_drogas                  ,
 		tiene_planificacion_sexual        
-	) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70,$71,$72,$73,$74)`
+	) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60,$61,$62,$63,$64,$65,$66,$67,$68,$69,$70,$71,$72,$73,$74) RETURNING id`
 	_, err_query := db.Exec(ctx, query,
 		input_historia_clinica.Id,
 		input_historia_clinica.IdMedico,
@@ -170,8 +170,8 @@ func Pg_Create(input_historia_clinica *models.HistoriaClinica) error {
 	)
 
 	if err_query != nil {
-		return err_query
+		return input_historia_clinica.Id, err_query
 	}
 
-	return nil
+	return input_historia_clinica.Id, nil
 }
